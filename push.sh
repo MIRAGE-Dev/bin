1cc3a25b05e9cf48842dafe66839d3abac0ad4c0
franco-c <tjrdnjs33936@gmail.com>
2013-07-06 02:02:47 +0900

    Bin: shit
    Change-Id: Ic475285b4b14b52f90a5762c2bb1fb3cbe7a9e77

---
+++ push.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# MIRAGE - xda@Des-franco
+FRANCO=https://github.com/Open-Wise
+. colors
+DIR=`pwd`
+DIR2=`echo $DIR | cut -c 24-50`
+REPO=https://github.com/Open-Wise/`echo $DIR2`
+APPS=`echo $DIR | cut -c 38-50`
+
+#     echo -e $CL_CYN"Pushing commits to "$CL_RST""remote="$CL_CYN"franco" "$CL_RST""branch="$CL_CYN"mirage""$CL_RST
+#     echo -e "REPOSITORY: "$CL_CYN"`echo $REPO`"$CL_RST""
+
+# Git configurations
+BRANCH=`cat .git/HEAD | cut -c 17-30`
+
+if [ "$REPO" == "$FRANCO/frameworks/base" ]; then
+     REPO=$FRANCO/frameworks_base_slim
+     echo -e $CL_YLW"Pushing commits to "$CL_RST""remote="$CL_CYN"franco" "$CL_RST""branch="$CL_CYN"$BRANCH""$CL_RST
+     echo -e "REPOSITORY: "$CL_YLW"`echo $REPO`"$CL_RST""
+     git push -u franco $BRANCH
+elif [ "$REPO" == "$FRANCO/packages/apps/$APPS" ]; then
+     REPO=$FRANCO/packages_apps_$APPS
+     echo -e $CL_YLW"Pushing commits to "$CL_RST""remote="$CL_CYN"franco" "$CL_RST""branch="$CL_CYN"$BRANCH""$CL_RST
+     echo -e "REPOSITORY: "$CL_YLW"`echo $REPO`"$CL_RST""
+     git push -u franco $BRANCH
+else
+     echo -e $CL_YLW"Pushing commits to "$CL_RST""remote="$CL_CYN"franco" "$CL_RST""branch="$CL_CYN"$BRANCH""$CL_RST
+     echo -e "REPOSITORY: "$CL_YLW"`echo $REPO`"$CL_RST""
+     git push -u franco $BRANCH
+fi
